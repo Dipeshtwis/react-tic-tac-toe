15e35dac2539a1fce6038cad151e680d2c95fc6a
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-12-01 21:26:26 +0530

    Add setState functionality on the board

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
   render() {
     return (
-      <button className="square" onClick={() => alert('click')}}>
-        {this.props.value}
+      <button className="square" 
+      onClick={() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
       </button>
     );
   }
